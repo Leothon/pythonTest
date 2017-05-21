715e560353d8d5847b574e029f78d6f203f1c478
Leothon Wong <langmancaizi@gmail.com>
2017-05-21 22:10:08 +0800

    just fuck a cheater

---
+++ Justreleasesomeangry.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+def fucking():
+    print "盗窃我QQ号的傻逼骗子，我操你妈！"
+
+def die():
+    print "盗窃我QQ号的傻逼骗子，你死全家！"
+
+def main():
+    fucking()
+    die()
+
+if __name__ == '__main__':
+    main()
